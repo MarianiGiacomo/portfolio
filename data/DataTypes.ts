a8adf4acf4f7da06d9e4487224f71742b99b507d
Giacomo Mariani <giacomo.mariani@gmail.com>
2024-12-15 01:22:40 +0200

    add components and data types

---
+++ data/DataTypes.ts
@@ -0,0 +1,18 @@
+export type Project = {
+  img: {
+		width: number,
+		height: number,
+		src: string,
+		alt: string
+	},
+	short: string,
+	title: string,
+	intro: string,
+	technologies: {
+		frontend?: string[],
+		backend?: string[]
+	},
+	links: {
+		[key: string]: string
+	}
+}
