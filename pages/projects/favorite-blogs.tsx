1eabe8dc44bf1fbe8443b5f4c905cdcedefcc04a
Giacomo Mariani <giacomo.mariani@gmail.com>
2024-12-16 23:26:33 +0200

    change pages/favorite-blogs to tsx

--- pages/projects/favorite-blogs.tsx
+++ pages/projects/favorite-blogs.tsx
@@ -1,12 +1,14 @@
+import React from 'react'
 import Layout from '../../components/container/Layout'
 import WebApp from '../../components/container/Webapp'
 import data from '../../data/projects/favorite-blogs'
 
-export default function FavoriteBlogs() {
+const FavoriteBlogs: React.FC = () => {
 	return ( 
 		<Layout>
 			<WebApp data={data} />
 		</Layout>
 	)
-
 }
+
+export default FavoriteBlogs
