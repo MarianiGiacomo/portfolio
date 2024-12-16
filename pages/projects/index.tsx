3069274155fabd353f2e9a15eb5bed08f9be54b4
Giacomo Mariani <giacomo.mariani@gmail.com>
2024-12-17 00:02:52 +0200

    change pages/projects root page to tsx

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -5,10 +5,10 @@ import Tag from '../../components/presentational/Tag'
 import { extractUniqueTech } from '../../helpers'
 
 import data from '../../data'
-import { boldName } from '../../helpers'
+import React from 'react'
 
-export default function Projects() {
-	const { firstName, lastName, home, profileImg, webApps } = { ...data }
+const Projects: React.FC = () => {
+	const { projects } = { ...data }
 
   return (
 		<Layout home>
@@ -16,9 +16,9 @@ export default function Projects() {
 				<h2 className="text-xl font-medium mb-1 mt-3 text-center">Personal Projects</h2>
 				<div className="sm:flex sm:flex-wrap gap-5 justify-between mt-8 mb-5 mr-auto ml-auto w-11/12">
 					{
-						Object.entries(webApps).map( (e, i) => {		
+						Object.entries(projects).map( (e, i) => {		
 							return <Card 
-								link={ `/projects/${e[0]}` }
+								link={`/projects/${e[0]}` }
 								heading={e[1].title?? ''}
 								text={e[1].short?? ''}
 								key={i}
@@ -31,7 +31,7 @@ export default function Projects() {
 			<section>
 				<h2 className="text-xl font-medium mb-1 mt-3 text-center">Technologies</h2>
 				<div className="flex flex-wrap mb-10 mt-3">
-					{ Array.from(extractUniqueTech(webApps)).concat('SQL').map( (t, i) => <Tag key={i} text={t} />)}
+					{ Array.from(extractUniqueTech(projects)).concat('SQL').map( (t, i) => <Tag key={i} text={t} />)}
 				</div>
 			</section>
 
@@ -39,3 +39,5 @@ export default function Projects() {
   )
 
 }
+
+export default Projects
