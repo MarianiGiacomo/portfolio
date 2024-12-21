48c72ea6624dc6e11d48108b65eb89eb19588dc4
Giacomo Mariani <giacomo.mariani@gmail.com>
2024-12-21 13:21:16 +0200

    change page/portfolio.js to tsx and use project page container

---
+++ pages/projects/portfolio.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from 'react'
+import data from '../../data/projects/portfolio'
+import ProjectPage from '../../components/container/ProjectPage'
+
+export default (): ReactElement => <ProjectPage data={data} /> 
