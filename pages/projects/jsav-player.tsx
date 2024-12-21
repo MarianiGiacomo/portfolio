1150cfa7c5a37c671ed2319015cbc3e1752b370a
Giacomo Mariani <giacomo.mariani@gmail.com>
2024-12-21 13:01:38 +0200

    change jsav-player.js to tsx

--- pages/projects/jsav-player.tsx
+++ pages/projects/jsav-player.tsx
@@ -2,7 +2,7 @@ import Layout from '../../components/container/Layout'
 import WebApp from '../../components/container/Webapp'
 import data from '../../data/projects/jsav-player'
 
-export default function JsavPlayer() {
+const JsavPlayer: React.FC = () => {
 	return ( 
 		<Layout>
 			<WebApp data={data} />
@@ -10,3 +10,5 @@ export default function JsavPlayer() {
 	)
 
 }
+
+export default JsavPlayer
