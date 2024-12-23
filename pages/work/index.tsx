fbe1824bb7fce1385c0a3dfe4882b5134cda2a6f
Giacomo Mariani <giacomo.mariani@gmail.com>
2024-12-24 00:49:49 +0200

    add work, skills, courses and certs subpages index page

---
+++ pages/work/index.tsx
@@ -0,0 +1,22 @@
+import Layout from '../../components/container/Layout';
+
+import React from 'react';
+import { NextPageWithLayout } from '../_app';
+
+const WorkPage: NextPageWithLayout = () => {
+
+  return (
+    <>
+      <section>
+        <h2 className='mb-1 mt-3 text-center text-xl font-medium'>
+          Work
+        </h2>
+      </section>
+    </>
+  );
+};
+
+WorkPage.getLayout = function getLayout(page: React.ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+export default WorkPage;
